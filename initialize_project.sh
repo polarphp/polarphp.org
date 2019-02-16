740c48687a92488356ba2945c46c5821e6646c49
zzusoftboy <zzu_softboy@163.com>
2019-02-16 22:22:02 +0800

    add submodule

--- initialize_project.sh
+++ initialize_project.sh
@@ -1,6 +1,2 @@
-if [ ! -d pdk-ide-auto-complete ]
-then
-    git clone polarphp:polarphp/pdk-ide-auto-complete
-    cd pdk-ide-auto-complete
-    ./initialize_project.sh
-fi
+git submodule init
+git submodule update
