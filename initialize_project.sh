4050d8ca08444d842995768805229b5e4407993e
zzusoftboy <zzu_softboy@163.com>
2019-02-16 23:42:46 +0800

    submodule update

--- initialize_project.sh
+++ initialize_project.sh
@@ -2,5 +2,5 @@ git submodule update --init --recursive
 cd pdk-ide-auto-complete
 ./initialize_project.sh
 cd ../
-ln -s node_modules/hexo-generator-php-apidoc hexo-generator-php-apidoc
-ln -s node_modules/hexo-generator-cpp-apidoc hexo-generator-cpp-apidoc
+[ ! -s hexo-generator-php-apidoc ] && ln -s node_modules/hexo-generator-php-apidoc hexo-generator-php-apidoc
+[ ! -s hexo-generator-cpp-apidoc ] && ln -s node_modules/hexo-generator-cpp-apidoc hexo-generator-cpp-apidoc
